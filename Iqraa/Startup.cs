f3e99edfa474e46296ae3427534d337779118d9a
Ahmed3d <Ahmed3d@ASSEMPC>
2020-12-26 13:50:34 +0200

    Add project files.

---
+++ Iqraa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Iqraa.Startup))]
+namespace Iqraa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
