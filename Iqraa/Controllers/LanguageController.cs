92aa0b94b1badab86c0e658d66f6665388674c0d
mohamedsamy <mohamedsamy@DESKTOP-4FDJ5MU>
2021-01-10 00:38:26 +0200

    Language Changes

---
+++ Iqraa/Controllers/LanguageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Iqraa.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Change(string LanguageAbbreviation)
+        {
+            if(LanguageAbbreviation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(LanguageAbbreviation);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(LanguageAbbreviation);
+
+            }
+            HttpCookie Cookie = new HttpCookie("Language");
+            Cookie.Value = LanguageAbbreviation;
+            Response.Cookies.Add(Cookie);
+
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
--- Iqraa/Global.asax.cs
+++ Iqraa/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -17,5 +19,29 @@ namespace Iqraa
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            HttpCookie Cookie = HttpContext.Current.Request.Cookies["Language"];
+            if (Cookie != null && Cookie.Value != null)
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(Cookie.Value);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Cookie.Value);
+
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+            }
+            //    HttpContext context = HttpContext.Current;
+            //var languageSession = "en";
+            //if (context != null && context.Session != null)
+            //{
+            //    languageSession = context.Session["lang"] != null ? context.Session["lang"].ToString() : "en";
+            //}
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo(languageSession);
+            //Thread.CurrentThread.CurrentCulture = new CultureInfo(languageSession);
+        }
+
     }
 }
