261067a8d8fafbf1cf8036105824d0f431d23bbd
mohamedsamy871 <mohamedsamy871@hotmail.com>
2021-01-21 18:31:11 +0200

    making mail notification to website admin at message coming

--- Iqraa/Controllers/ContactUsController.cs
+++ Iqraa/Controllers/ContactUsController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -22,12 +23,48 @@ namespace Iqraa.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult AddMessage(ContactUs ComingMessage)
         {
+            db.ContactUs.Add(ComingMessage);
+            db.SaveChanges();
             //if (!ModelState.IsValid)
             //{
             //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Please,Try Again");
             //}
-            db.ContactUs.Add(ComingMessage);
-            db.SaveChanges();
+            var fromAddress = new MailAddress("info@iqraa-center.com", "موقع اقرأ");
+            var toAddress = new MailAddress("quran.literacycenter@gmail.com", "ادارة موقع اقرأ");
+            var fromPassword = "info@123456";
+            var subject = "رسالة واردة الي موقع اقرأ";
+            var body = ComingMessage.Message;
+            var smtp = new SmtpClient("mail.iqraa-center.com") /*587*/
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("info@iqraa-center.com", fromPassword),
+                EnableSsl = false,
+                Port = 8889
+
+            };
+
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body,
+                Priority = MailPriority.High,
+                IsBodyHtml = true,
+
+            }
+                  )
+            {
+                //smtp.Send(message);
+                try
+                {
+                    smtp.Send(message);
+                    Response.Write("Email Send successfully");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write(ex.Message);
+                }
+            }
+            
             return RedirectToAction("Index");
         }
     }
